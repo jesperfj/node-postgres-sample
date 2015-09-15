45d965f22faa1a004e9ed8cc5914f6d199444e9a
Jesper Joergensen <jesper@heroku.com>
2015-09-14 23:54:46 -0700

    basic stuff working now

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var db = app.get('db');
 
-// Set up a new product
-router.get('/_new', function(req, res, next) {
-  res.render('newproduct', { title: 'New Product' });
+var productAttributes = [ 
+    { label: 'Name', name: 'name' },
+    { label: 'Description', name: 'description' },
+    { label: 'Dimensions', name: 'dimensions' }
+];
+
+// Find products
+router.get('/', function(req, res, next) {
+  if(!req.query.q) {
+    res.render('products', {
+      tab: 'products',
+      title: 'Find Products'
+    });
+  } else {
+    req.app.get('db').product.searchDoc({
+      keys: productAttributes.map(function(v) { return v.name }),
+      term: req.query.q
+    }, function(err,docs) {
+      console.log(err);
+      console.log(docs);
+      res.render('products', { 
+        tab: 'products', 
+        title: 'Find Products',
+        q: req.query.q,
+        attributes: productAttributes,
+        results: docs 
+      });
+    });
+  }
 });
 
+// Render new product page
+router.get('/_new', function(req, res, next) {
+  res.render('new-product', { 
+    tab: 'new-product', 
+    title: 'New Product', 
+    attributes: productAttributes
+  });
+});
 
-// Set up a new product
+// Create a new product
 router.post('/', function(req, res, next) {
-  console.log(req.body)
-  res.send('respond with a resource');
+  req.app.get('db').saveDoc("product",req.body, function(err,result) {
+    if(err) {
+        console.log(err);
+        res.send(500);
+    } else {
+        console.log("Saved: "+req.body);
+        console.log("Response: "+JSON.stringify(result));
+        console.log("Redirecting to /products/"+result.id)
+        res.redirect("/products/"+result.id);
+    }
+  })
 });
 
 module.exports = router;
