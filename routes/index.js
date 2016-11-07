2c2a353545ad67276f41482d78d28f35ecdd4a1e
Jesper Joergensen <jesper@heroku.com>
2016-11-06 22:05:42 -0800

    add /health

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/health', function(req,res,next) {
+  res.send(200);
+});
+
 module.exports = router;
